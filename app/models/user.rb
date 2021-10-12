098e9bb0c6c814b9c3fe0f64a684b19f79fb8c7c
Weston Ellis <ellisweston112@gmail.com>
2021-10-12 14:03:40 -0600

    Write relationships and tests

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@ class User < ApplicationRecord
   has_many :friends, through: :friendships
   has_many :inverse_friendships, class_name: 'Friendship', foreign_key: 'friend_id'
   has_many :inverse_friends, through: :inverse_friendships, source: :user
+  has_many :user_showings, dependent: :destroy
+  has_many :showings, through: :user_showings
 
   validates :email, presence: true, uniqueness: true
   validates :password_digest, presence: true
--- spec/models/showing_spec.rb
+++ spec/models/showing_spec.rb
@@ -1,10 +1,9 @@
 require 'rails_helper'
 
 RSpec.describe Showing do
-  it 'exists' do
-    party = Showing.create!(movie_title: "yo", duration: 69, day: "asd", start_time: "456")
-
-    expect(party.movie_title).to eq("yo")
+  describe 'relationships' do
+    it {should have_many(:user_showings)}
+    it {should have_many(:users).through(:user_showings)}
   end
   describe 'validations' do
     it {should validate_presence_of(:movie_title)}
