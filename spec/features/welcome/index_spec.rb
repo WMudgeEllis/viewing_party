cab23373982d4b1be231d6b1fd47141ee844fe32
Weston Ellis <ellisweston112@gmail.com>
2021-10-13 15:37:49 -0600

    Write test for login form

--- spec/features/welcome/index_spec.rb
+++ spec/features/welcome/index_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe 'Welcome Page' do
   before(:each) do
     @welcome_message = "Welcome to viewing party! Be sure to grab some extra popcorn."
     @description = "Viewing party is an application that allows you to schedule showings with your friends or anyone you'd like. If you are new here be sure to register with us!"
-    @user = User.create!(email: "sunlesskhan", password_digest: "flipreset")
+    @user = User.create!(email: "sunlesskhan", password: "flipreset")
   end
 
   it 'shows welcome message and description' do
@@ -14,17 +14,22 @@ RSpec.describe 'Welcome Page' do
     expect(page).to have_content(@description)
   end
 
-  xit 'shows a button to register as a new user' do
+  it 'shows a button to register as a new user' do
     visit root_path
     click_on "Register"
 
-    expect(current_path).to eq(new_user_path)
+    expect(current_path).to eq(registration_path)
   end
 
-  it 'shows a button to login' do
+  it 'logs the user in' do
     visit root_path
+
+    fill_in :email, with: @user.email
+    fill_in :password, with: @user.password
+
     click_on "Login"
 
-    expect(current_path).to eq(login_path)
+    expect(current_path).to eq(dashboard_path)
+    expect(page).to have_content("Login Successful")
   end
 end
