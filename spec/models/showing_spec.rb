8f4463aac1ccc713e8f8ccbe50706be55cb268a1
Weston Ellis <ellisweston112@gmail.com>
2021-10-14 17:21:57 -0600

    Add method test

--- spec/models/showing_spec.rb
+++ spec/models/showing_spec.rb
@@ -12,14 +12,21 @@ RSpec.describe Showing do
     it {should validate_presence_of(:start_time)}
   end
 
-  it 'can get host name' do
+  before :each do
     @user = User.create!(email: "sunlesskhan@kahn.com", password: "flipreset")
     @user2 = User.create!(email: "scar@sa.net", password: "face")
+    @user3 = User.create!(email: "balderdash@yada.com", password: "123")
     @showing = Showing.create!(movie_title: 'star warz', duration: 120, day: '1/2/1993', start_time: '1800')
     UserShowing.create!(user: @user, showing: @showing, user_hosting: false)
     UserShowing.create!(user: @user2, showing: @showing, user_hosting: true)
-    UserShowing.create!(user: @user, showing: @showing, user_hosting: false)
+    UserShowing.create!(user: @user3, showing: @showing, user_hosting: false)
 
+  end
+  it 'can get host name' do
     expect(@showing.host).to eq(@user2.email)
+  end
+
+  it 'can get invitees' do
+    expect(@showing.invitees).to eq([@user, @user3])
   end
 end
