49d68bd79d47108fea691040d8e18f13fbf9b5c9
Weston Ellis <ellisweston112@gmail.com>
2021-10-14 14:57:47 -0600

    Write sad search

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,8 +2,11 @@ class FriendshipsController < ApplicationController
   def create
     friend = User.find_by(email: params[:find_friend])
     user = User.find(session[:user_id])
-    user.friendships.create(friend_id: friend.id)
-    #require "pry"; binding.pry
+    if friend
+      user.friendships.create(friend_id: friend.id)
+    else
+      flash[:error] = 'User does not exist'
+    end
     redirect_to dashboard_path
   end
 end
