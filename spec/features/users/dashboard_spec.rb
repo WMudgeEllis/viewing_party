28e99554a575f90814394e9b38a2e9dadff6bfe5
Weston Ellis <ellisweston112@gmail.com>
2021-10-14 16:00:48 -0600

    Write test for invited showings

--- spec/features/users/dashboard_spec.rb
+++ spec/features/users/dashboard_spec.rb
@@ -2,86 +2,117 @@ require 'rails_helper'
 
 RSpec.describe 'user dashboard page' do
 
-  before :each do
-    @user = User.create!(email: "sunlesskhan", password: "flipreset")
-    @user2 = User.create!(email: "scar", password: "face")
-  end
-
-  it 'can log out' do
-    visit root_path
-
-    fill_in :email, with: @user.email
-    fill_in :password, with: @user.password
-
-    click_on  "Login"
-
-    expect(current_path).to eq(dashboard_path)
-
-    click_on 'Log Out'
-
-    expect(current_path).to eq(root_path)
-    expect(page).to have_content('You have been logged out')
-  end
-
-
-  it 'can find a friend' do
-    visit root_path
-
-    fill_in :email, with: @user.email
-    fill_in :password, with: @user.password
-
-    click_on  "Login"
-
-    expect(page).to_not have_content(@user2.email)
-    expect(@user.friends).to eq([])
-
-    within('#friend-search') do
-      fill_in :find_friend, with: @user2.email
-      click_button "Add"
+  describe 'logout and friend functionality' do
+    before :each do
+      @user = User.create!(email: "sunlesskhan", password: "flipreset")
+      @user2 = User.create!(email: "scar", password: "face")
     end
 
-    @user.reload
-    expect(@user.friends).to eq([@user2])
-    expect(current_path).to eq(dashboard_path)
-    expect(page).to have_content(@user2.email)
+    it 'can log out' do
+      visit root_path
 
+      fill_in :email, with: @user.email
+      fill_in :password, with: @user.password
+
+      click_on  "Login"
+
+      expect(current_path).to eq(dashboard_path)
+
+      click_on 'Log Out'
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content('You have been logged out')
+    end
+
+
+    it 'can find a friend' do
+      visit root_path
+
+      fill_in :email, with: @user.email
+      fill_in :password, with: @user.password
+
+      click_on  "Login"
+
+      expect(page).to_not have_content(@user2.email)
+      expect(@user.friends).to eq([])
+
+      within('#friend-search') do
+        fill_in :find_friend, with: @user2.email
+        click_button "Add"
+      end
+
+      @user.reload
+      expect(@user.friends).to eq([@user2])
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content(@user2.email)
+
+    end
+
+    it 'shows message when friends list is empty' do
+      message = "You don't have any friends. Add one!"
+      visit root_path
+
+      fill_in :email, with: @user.email
+      fill_in :password, with: @user.password
+
+      click_on  "Login"
+
+      expect(page).to have_content(message)
+    end
+
+    it 'makes sure only authenticated users can access dashboard' do
+      visit dashboard_path
+
+      expect(current_path).to eq(root_path)
+      expect(page).to have_content('please log in or create an account')
+    end
+
+    it 'can fail to find a friend' do
+      visit root_path
+
+      fill_in :email, with: @user.email
+      fill_in :password, with: @user.password
+
+      click_on  "Login"
+
+      fill_in :find_friend, with: 'nofriend@sad.com'
+
+      click_button 'Add'
+
+      @user.reload
+      expect(current_path).to eq(dashboard_path)
+      expect(page).to have_content('User does not exist')
+      expect(@user.friends).to eq([])
+    end
   end
 
-  it 'shows message when friends list is empty' do
-    message = "You don't have any friends. Add one!"
-    visit root_path
+  describe 'showing functionality' do
+    before :each do
+      @user = User.create!(email: "sunlesskhan@kahn.com", password: "flipreset")
+      @user2 = User.create!(email: "scar@sa.net", password: "face")
+      @user3 = User.create!(email: 'sleeps@zz.com', password: 'yawn')
+      @user4 = User.create!(email: 'wakefull@dud.com', password: 'coffee')
+      @showing = Showing.create!(movie_title: 'star warz', duration: 120, day: '1/2/1993', start_time: '1800')
+      @showing2 = Showing.create!(movie_title: 'return of the star warz', duration: 120, day: '3/9/2018', start_time: '0800')
+      @user.friendships.create!(friend_id: @user2.id)
+      @user.friendships.create!(friend_id: @user3.id)
+      @user4.friendships.create!(friend_id: @user.id)
+    end
 
-    fill_in :email, with: @user.email
-    fill_in :password, with: @user.password
+    it 'can show all viewing parties invited' do
+      allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+      UserShowing.create!(user: @user, showing: @showing, user_hosting: false)
+      UserShowing.create!(user: @user, showing: @showing2, user_hosting: false)
 
-    click_on  "Login"
+      visit dashboard_path
 
-    expect(page).to have_content(message)
+      expect(page).to have_content(@showing.movie_title)
+      expect(page).to have_content(@showing.day)
+      expect(page).to have_content(@showing.start_time)
+      expect(page).to have_content(@showing2.movie_title)
+      expect(page).to have_content(@showing2.day)
+      expect(page).to have_content(@showing2.start_time)
+    end
+
   end
-
-  it 'makes sure only authenticated users can access dashboard' do
-    visit dashboard_path
-
-    expect(current_path).to eq(root_path)
-    expect(page).to have_content('please log in or create an account')
-  end
-
-  it 'can fail to find a friend' do
-    visit root_path
-
-    fill_in :email, with: @user.email
-    fill_in :password, with: @user.password
-
-    click_on  "Login"
-
-    fill_in :find_friend, with: 'nofriend@sad.com'
-
-    click_button 'Add'
-
-    @user.reload
-    expect(current_path).to eq(dashboard_path)
-    expect(page).to have_content('User does not exist')
-    expect(@user.friends).to eq([])
-  end
-
 end
