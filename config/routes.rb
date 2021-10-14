dcb3a473fc712edd937380711a67e04927231429
Weston Ellis <ellisweston112@gmail.com>
2021-10-13 18:32:37 -0600

    Restrict extra routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root "welcome#index"
-  resources :users
+  resources :users, only: [:create, :new, :show]
   post '/login', to: 'sessions#create'
   delete '/logout', to: 'sessions#destroy', as: 'logout'
   get '/registration', to: 'users#new'
