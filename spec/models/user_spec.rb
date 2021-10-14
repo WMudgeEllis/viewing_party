fdda39a38356e4ce4b06cbb16672c54bd1250cf2
Weston Ellis <ellisweston112@gmail.com>
2021-10-13 18:12:41 -0600

    Add friends relationsip test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -5,6 +5,8 @@ RSpec.describe User do
   describe 'relationships' do
     it {should have_many(:user_showings)}
     it {should have_many(:showings).through(:user_showings)}
+    it {should have_many(:friendships)}
+    it {should have_many(:friends).through(:friendships)}
   end
   describe 'validations' do
     it {should validate_presence_of(:email)}
