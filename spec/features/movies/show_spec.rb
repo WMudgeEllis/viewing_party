5da31b09d15023a26770112966dc559c3d473110
Weston Ellis <ellisweston112@gmail.com>
2021-10-15 15:17:15 -0600

    Write test for show page

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -2,5 +2,14 @@ require "rails_helper"
 
 
 RSpec.describe 'movie show page' do
-  
+
+  it 'has a button to new viewing party' do
+    visit '/movies/1167'
+
+    expect(page).to have_button('Create Viewing Party')
+
+    click_button 'Create Viewing Party'
+
+    expect(current_path).to eq(new_showing_path)
+  end
 end
