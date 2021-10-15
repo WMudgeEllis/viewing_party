52f32c6cc8c70363d6f278bd7c6c795017b79f83
Weston Ellis <ellisweston112@gmail.com>
2021-10-15 15:24:00 -0600

    Add authorization to tests

--- spec/features/movies/show_spec.rb
+++ spec/features/movies/show_spec.rb
@@ -3,6 +3,11 @@ require "rails_helper"
 
 RSpec.describe 'movie show page' do
 
+  before :each do
+    @user = User.create!(email: "sunlesskhan@kahn.com", password: "flipreset")
+    allow_any_instance_of(ApplicationController).to receive(:current_user).and_return(@user)
+  end
+
   it 'has a button to new viewing party' do
     visit '/movies/1167'
 
