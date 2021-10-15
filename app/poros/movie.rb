5b4fea01951f59518fd66519c83771515efd0252
Weston Ellis <ellisweston112@gmail.com>
2021-10-15 17:08:43 -0600

    Rename summary to overview

--- app/poros/movie.rb
+++ app/poros/movie.rb
@@ -1,11 +1,11 @@
 class Movie
-  attr_reader :genres, :title, :vote_average, :summary
+  attr_reader :genres, :title, :vote_average, :overview
 
   def initialize(data)
     @genres = data[:genres]
     @title = data[:title]
     @vote_average = data[:vote_average]
-    @summary = data[:summary]
+    @overview = data[:summary]
     @runtime = data[:runtime]
   end
 
