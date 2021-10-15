1d25c86054e08ffa89626b5ba5a50ae870513101
Weston Ellis <ellisweston112@gmail.com>
2021-10-15 17:06:49 -0600

    Write movie_info

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,22 +1,28 @@
 class MovieService
-  def self.get_top_rated_movies(page)
-    response = conn.get('/3/movie/top_rated') do |f|
-      f.params['page'] = page
-    end
-    parse_json(response)[:results]
-  end
-
-  def self.top_40_movies
-    get_top_rated_movies(1) + get_top_rated_movies(2)
-  end
-
-  def self.conn
-    Faraday.new('https://api.themoviedb.org') do |f|
-      f.params['api_key'] = ENV['movie_key']
-    end
-  end
-
-  def self.parse_json(response)
+  URL = 'https://api.themoviedb.org/3/movie/'
+  # def self.get_top_rated_movies(page)
+  #   response = conn.get('/3/movie/top_rated') do |f|
+  #     f.params['page'] = page
+  #   end
+  #   parse_json(response)[:results]
+  # end
+  #
+  # def self.top_40_movies
+  #   get_top_rated_movies(1) + get_top_rated_movies(2)
+  # end
+  #
+  # def self.conn
+  #   Faraday.new('https://api.themoviedb.org') do |f|
+  #     f.params['api_key'] = ENV['movie_key']
+  #   end
+  # end
+  #
+  # def self.parse_json(response)
+  #   JSON.parse(response.body, symbolize_names: true)
+  # end
+  def self.movie_info(movie_id)
+    # require "pry"; binding.pry
+    response = Faraday.get(URL + movie_id, {api_key: ENV['movie_key']})
     JSON.parse(response.body, symbolize_names: true)
   end
 end
