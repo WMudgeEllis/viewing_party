488aab8732916a0e4b9e95a66579fd2fc1b187ab
Weston Ellis <ellisweston112@gmail.com>
2021-10-16 10:00:42 -0600

    Write CastService methods

---
+++ app/services/cast_service.rb
@@ -0,0 +1,12 @@
+class CastService
+  URL = 'https://api.themoviedb.org/3/movie/'
+
+  def self.first_10_cast(movie_id)
+    cast_info(movie_id)[:cast].map { |cast| CastMember.new(cast) }[0..9]
+  end
+
+  def self.cast_info(movie_id)
+    response = Faraday.get("#{URL+movie_id}/credits", {api_key: ENV['movie_key']})
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
