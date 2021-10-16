7d43819f32668bd48fd29ea1dd07173b698d340d
Weston Ellis <ellisweston112@gmail.com>
2021-10-16 10:48:42 -0600

    Add reviews to view

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -6,5 +6,6 @@ class MoviesController < ApplicationController
   def show
     @movie = Movie.new(MovieService.movie_info(params[:id]))
     @cast = CastService.first_10_cast(params[:id])
+    @reviews = ReviewService.reviews(params[:id])
   end
 end
