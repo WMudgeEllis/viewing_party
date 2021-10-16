46a8d5e10cc30b58215e6d02781ad95cd02a6922
Weston Ellis <ellisweston112@gmail.com>
2021-10-16 10:17:49 -0600

    Write review class"

--- spec/poros/review_spec.rb
+++ spec/poros/review_spec.rb
@@ -6,7 +6,7 @@ RSpec.describe Review do
             author: "tmdb73913433",
             content: "Make way for the best film ever made people. **Make way.**"
     }
-    review = Review.nea(data)
+    review = Review.new(data)
 
     expect(review.author).to eq("tmdb73913433")
     expect(review.content).to eq("Make way for the best film ever made people. **Make way.**")
