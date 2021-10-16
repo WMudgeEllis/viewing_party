f097b8bd884bb6803a07cf5b4dd41d6ec7004bb3
Weston Ellis <ellisweston112@gmail.com>
2021-10-16 10:35:49 -0600

    Add cassette expiration

--- spec/services/review_service_spec.rb
+++ spec/services/review_service_spec.rb
@@ -3,10 +3,12 @@ require "rails_helper"
 RSpec.describe ReviewService do
 
   it 'can fetch review info' do
-    expect(ReviewService.reviews('278')).to be_a(Array)
-    expect(ReviewService.reviews('278').length).to eq(7)
-    expect(ReviewService.reviews('278').first).to be_a(Review)
-    expect(ReviewService.reviews('278').first.name).to eq("elshaarawy")
+    VCR.use_cassette('movie_reviews', re_record_interval: 1.days) do
+      expect(ReviewService.reviews('278')).to be_a(Array)
+      expect(ReviewService.reviews('278').length).to eq(7)
+      expect(ReviewService.reviews('278').first).to be_a(Review)
+      expect(ReviewService.reviews('278').first.name).to eq("elshaarawy")
+    end
   end
 
 end
