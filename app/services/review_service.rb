a8e0e03fced36e1690f74f7b05fe134579255622
Weston Ellis <ellisweston112@gmail.com>
2021-10-16 10:38:59 -0600

    Write ReviewService

---
+++ app/services/review_service.rb
@@ -0,0 +1,12 @@
+class ReviewService
+  URL = 'https://api.themoviedb.org/3/movie/'
+
+  def self.reviews(movie_id)
+    parse_json(movie_id)[:results].map { |review| Review.new(review) } 
+  end
+
+  def self.parse_json(movie_id)
+    response = Faraday.get("#{URL+movie_id}/reviews", {api_key: ENV['movie_key']})
+    JSON.parse(response.body, symbolize_names: true)
+  end
+end
--- spec/services/review_service_spec.rb
+++ spec/services/review_service_spec.rb
@@ -7,7 +7,7 @@ RSpec.describe ReviewService do
       expect(ReviewService.reviews('278')).to be_a(Array)
       expect(ReviewService.reviews('278').length).to eq(7)
       expect(ReviewService.reviews('278').first).to be_a(Review)
-      expect(ReviewService.reviews('278').first.name).to eq("elshaarawy")
+      expect(ReviewService.reviews('278').first.author).to eq("elshaarawy")
     end
   end
 
