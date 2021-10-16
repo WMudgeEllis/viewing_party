510bdc882a23d31c87f2bffde1bdf4e46fbe01a6
Weston Ellis <ellisweston112@gmail.com>
2021-10-16 14:59:41 -0600

    Add basic showing creation

--- app/controllers/showings_controller.rb
+++ app/controllers/showings_controller.rb
@@ -6,4 +6,20 @@ class ShowingsController < ApplicationController
     @showing = Showing.new
     @friends = current_user.friends
   end
+
+  def create
+    new_showing = Showing.new(showing_params)
+
+    if new_showing.save
+      current_user.user_showings.create!(showing: new_showing, user_hosting: true)
+      redirect_to dashboard_path
+    end
+
+  end
+
+  private
+
+  def showing_params
+    params.require(:showing).permit(:movie_title, :duration, :day, :start_time)
+  end
 end
