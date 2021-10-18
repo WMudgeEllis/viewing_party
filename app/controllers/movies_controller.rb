32dfd226d051a3525552d3e81e66d67ac1404171
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-10-18 10:07:58 -0600

    add facade to controller

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,7 +1,13 @@
 class MoviesController < ApplicationController
   before_action :require_user
 
-  def index; end
+  def index
+    if params[:query].present?
+      @search = APIS::MoviesFacade.search_by_title(params[:query])
+    elsif params[:query].nil?
+      @movies = APIS::MoviesFacade.top_40_movies
+    end
+  end
 
   def show
     @movie = Movie.new(MovieService.movie_info(params[:id]))
