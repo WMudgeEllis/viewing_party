8d02eb49784a1b88d5058fb69a98f131792b5332
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-10-18 17:32:23 -0600

    movie facade

---
+++ app/facades/movies_facade.rb
@@ -0,0 +1,17 @@
+  class MoviesFacade
+    def self.top_40_movies
+      movies = MovieService.top_40_movies
+
+      movies.map do |movie|
+        Movie.new(movie)
+      end
+    end
+
+    def self.search_by_title(title)
+      service = MovieService.search_movie_by_title(title)
+
+      service[:results].map do |movie|
+        MovieResult.new(movie)
+      end
+    end
+  end
