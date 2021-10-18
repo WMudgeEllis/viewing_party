09e3aa2493570e5805052488ab7cf05dd0b58e97
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-10-18 10:08:07 -0600

    create movie facade

---
+++ app/facades/apis/movies_facade.rb
@@ -0,0 +1,19 @@
+module APIS
+  class MoviesFacade
+    def self.top_40_movies
+      movies = MovieService.top_40_movies
+
+      movies[:results].map do |movie|
+        Movie.new(movie)
+      end
+    end
+
+    def self.search_by_title(title)
+      service = MovieService.search_movie_by_title(title)
+
+      service[:results].map do |movie|
+        MovieResult.new(movie)
+      end
+    end
+  end
+end
