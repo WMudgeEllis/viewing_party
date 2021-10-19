46d8c8e039ec12ee21a54368d55108961a49d339
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-10-19 14:59:46 -0600

    added logic for duplicate friends/adding self

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,8 +2,13 @@ class FriendshipsController < ApplicationController
   def create
     friend = User.find_by(email: params[:find_friend])
     user = User.find(current_user.id)
-    if friend
-      user.friendships.create(friend_id: friend.id)
+
+    if user.friend?(params[:find_friend])
+      flash[:error] = "This user is already your friend."
+    elsif params[:find_friend] == user.email
+      flash[:error] = "You can't add yourself as a friend!"
+    elsif friend
+      user.friendships.create!(friend_id: friend.id)
     else
       flash[:error] = 'User does not exist'
     end
