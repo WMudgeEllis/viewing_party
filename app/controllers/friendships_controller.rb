5d2b08a04e32cbf7dea9eb8ebf3703839be468a0
Weston Ellis <ellisweston112@gmail.com>
2021-10-19 16:17:46 -0600

    Refactor

--- app/controllers/friendships_controller.rb
+++ app/controllers/friendships_controller.rb
@@ -2,15 +2,21 @@ class FriendshipsController < ApplicationController
   def create
     friend = User.find_by(email: params[:find_friend])
     user = User.find(current_user.id)
-    if user.friend?(params[:find_friend])
+    friend_flow(params[:find_friend], friend, user)
+    redirect_to dashboard_path
+  end
+
+  private
+
+  def friend_flow(find_friend, friend, user)
+    if user.friend?(find_friend)
       flash[:error] = 'This user is already your friend.'
-    elsif params[:find_friend] == user.email
+    elsif find_friend == user.email
       flash[:error] = "You can't add yourself as a friend!"
     elsif friend
       user.friendships.create!(friend_id: friend.id)
     else
       flash[:error] = 'User does not exist'
     end
-    redirect_to dashboard_path
   end
 end
