5c22fc76d52853a7022262328df9fc8eb3f9ae17
Weston Ellis <ellisweston112@gmail.com>
2021-10-19 11:56:02 -0600

    Refactor

--- app/facades/movies_facade.rb
+++ app/facades/movies_facade.rb
@@ -1,10 +1,14 @@
 class MoviesFacade
   def self.reviews(movie_id)
-    ReviewService.review_info(movie_id)[:results].map { |review| Review.new(review) }
+    ReviewService.review_info(movie_id)[:results].map do |review|
+       Review.new(review)
+    end
   end
 
   def self.first_10_cast(movie_id)
-    CastService.cast_info(movie_id)[:cast].map { |cast| CastMember.new(cast) }[0..9]
+    CastService.cast_info(movie_id)[:cast].map do |cast|
+      CastMember.new(cast)
+    end[0..9]
   end
 
   def self.top_40_movies
