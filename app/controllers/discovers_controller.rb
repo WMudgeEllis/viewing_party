99b44536452461350fa07932ea1ad6562a301824
Luis Arroyo <arroyoluis1516@yahoo.com>
2021-10-19 19:11:38 -0600

    monstrocity

--- app/controllers/discovers_controller.rb
+++ app/controllers/discovers_controller.rb
@@ -1,18 +1,10 @@
 class DiscoversController < ApplicationController
 
-  def index
-    @movies = if params[:query].present?
-                MoviesFacade.search_by_title(params[:query])
-              else
-                MoviesFacade.top_40_movies
-              end
-  end
-
   def show
     @movies = if params[:query].present?
                 MoviesFacade.search_by_title(params[:query])
               else
                 MoviesFacade.top_40_movies
               end
-  end 
+  end
 end
